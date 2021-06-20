078515240827ed718b03742a3ccbbf96089b4384
Will Farley <farley_w_@hotmail.com>
2021-06-20 20:16:59 +0100

    Adding working rds

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_db_instance" "default" {
+  identifier           = "will-demo-db-tf"
+  allocated_storage    = 10
+  engine               = "postgres"
+  engine_version       = "12.5"
+  instance_class       = "db.t2.micro"
+  name                 = "demoa"
+  username             = "postgres"
+  password             = "${var.password}"
+  parameter_group_name = "default.postgres12"
+  skip_final_snapshot  = true
+  db_subnet_group_name = "will-db-subnet"
+  vpc_security_group_ids = ["sg-016ba6c8b1dac48be"]
+  publicly_accessible = true
+}
+
+variable "password" {
+  description = "db_password"
+}
